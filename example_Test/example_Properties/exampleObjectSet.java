322a4218f41214c5633cad52331e74bba5ab583d
Suneel Kaushik S <45133346+suneel944@users.noreply.github.com>
2019-02-23 19:26:12 +0530

    Example Properties File

---
+++ example_Test/example_Properties/exampleObjectSet.java
@@ -0,0 +1,13 @@
+package example_Properties;
+
+import org.openqa.selenium.By;
+
+public class exampleObjectSet 
+	{
+	
+	//example template
+	public static By exampleOBJXPATH = By.xpath("xpathExpression");
+	public static By exampleOBJID = By.id("Element ID");
+	public static By exampleOBJCSS = By.cssSelector("Element Css Selector");
+	public static By exampleOBJCLASS = By.className("Element Class Name");
+	}
