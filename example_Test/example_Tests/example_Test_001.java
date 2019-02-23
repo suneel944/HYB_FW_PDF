bd533ce0fc5ffbc7fb12d723904fb54650dd7b91
Suneel Kaushik S <45133346+suneel944@users.noreply.github.com>
2019-02-23 19:27:29 +0530

    Test Case File

---
+++ example_Test/example_Tests/example_Test_001.java
@@ -0,0 +1,44 @@
+package example_Tests;
+
+import java.io.IOException;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import example_Pages.examplePage;
+import excelLib.ExcelLib;
+import jxl.read.biff.BiffException;
+
+public class example_Test_001
+{
+	//*******************************************************************************
+	@DataProvider
+	public Object[][] data() throws BiffException, IOException 
+	{
+		ExcelLib xl = new ExcelLib("Excel Sheet Name", this.getClass().getSimpleName());
+		return xl.getTestdata();
+	}
+	//*******************************************************************************
+	//Test Case Objective
+	/**
+	 * Test Case Objective
+	 */
+	private static String TestCaseObjective = "Validate Ordering of a product in Amazon website";
+	
+	//*******************************************************************************
+	@Test(dataProvider = "data")
+	/*Note: While providing the arguments, please make sure 
+	 * that the order of arguments passed inside run function 
+	 * should be same as the order respected in data sheet, 
+	 * otherwise script wont run
+	 * OR In simple words, "the order of the arguments must be respected"*/
+	
+	public void testRun(String url, String browserName, String search) throws Exception
+	{
+		//Instantiate the page
+		examplePage example = new examplePage();
+		
+		//Test case
+		example.beforemethod(this.getClass().getSimpleName(), TestCaseObjective, url);
+		example.exampleFunctionalityToBePerformed(url, browserName, search);
+	}
+	//*******************************************************************************
+}
