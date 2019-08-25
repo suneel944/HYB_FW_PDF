6183eabb591d4ae43b46e9b2d0045397bfabea43
Suneel Kaushik S <suneel944@gmail.com>
2019-08-26 03:09:20 +0530

    Updated Property Class

--- Amazon_Test/Amazon_Properties/OR_Common.java
+++ Amazon_Test/Amazon_Properties/OR_Common.java
@@ -9,9 +9,9 @@ public class OR_Common
 	
 	public static By btnSearch = By.xpath("(.//*[@type='submit'])[1]");
 	
-	public static By lnkFirstProduct = By.xpath("(.//h2[@data-attribute])[1]");
+	public static By lnkFirstProduct = By.xpath("(.//*[@data-component-type='s-product-image']//a)[1]");
 	
-	public static By weProductPageNavigationContainer = By.id("leftNavContainer");
+	public static By resultText = By.xpath(".//*[contains(text(),'results for')]");
 	
 	public static By btnBuyNow = By.id("buy-now-button");
 	
